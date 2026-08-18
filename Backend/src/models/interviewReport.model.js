8326a9e0917b658717f2616acac9502cc9450529
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-08-19 01:50:26 +0530

    feat: add user reference to interview report model

--- Backend/src/models/interviewReport.model.js
+++ Backend/src/models/interviewReport.model.js
@@ -81,7 +81,11 @@ const interviewReportSchema = new mongoose.Schema({
     technicalQuestions : [ technicalQuestionSchema ],
     behavioralQuestions : [ behavioralQuestionSchema ],
     skillGaps : [ skillGapsSchema ],
-    preparationPlan : [ preparationPlanSchema ] 
+    preparationPlan : [ preparationPlanSchema ],
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "user"
+    }
 }, {
     timestamps : true
 })
