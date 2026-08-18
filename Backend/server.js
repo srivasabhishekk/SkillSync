b9e21698ef214bc0042739538d39aa0b843dd162
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-08-18 13:03:36 +0530

    feat: connect application to MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,8 @@
-require('dotenv').config
+require('dotenv').config()
 const app = require('./src/app')
+const dbConnnection = require('./src/config/dbConnection')
+
+dbConnnection()
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
---
+++ Backend/src/config/dbConnection.js
@@ -0,0 +1,17 @@
+require('dotenv').config
+const mongoose = require('mongoose')
+
+async function dbConnection(){
+    const uri = process.env.MONGO_URI
+
+    try{
+        const connect = await mongoose.connect(uri)
+        console.log('Connected To Database')
+        console.log(connect.connection.name, connect.connection.host)
+    }catch(err){
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+module.exports = dbConnection
