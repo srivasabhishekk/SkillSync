a099359ca2967fb2188fe6f3df61236f4e0b212c
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-08-18 13:19:00 +0530

    feat: add user model and schema

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        unique : [ true, "username already taken"],
+        required : true
+    },
+
+    email : {
+        type : String,
+        unique : [ true, "accound already exists with this email"],
+        required : true
+    },
+
+    password : {
+        type : String,
+        required : true
+    }
+})
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel
