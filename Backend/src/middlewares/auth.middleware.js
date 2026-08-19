c2a77e9921a3d9ec46b26e8c928f4248dc9d14ad
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-08-19 14:13:58 +0530

    feat: add auth middleware for protected routes

---
+++ Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken')
+const blacklistTokenModel = require('../models/blacklist.model')
+
+async function authUser(req, res, next){
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message : "Token not provided!"
+        })
+    }
+
+    const isTokenBlacklisted = await blacklistTokenModel.findOne({ token })
+
+    if(isTokenBlacklisted){
+        return res.status(401).json({
+            message : "Invalid request, access denied!"
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    }catch(err){
+        return res.status(401).json({
+            message : "Invalid token"
+        })
+    }
+}
+
+module.exports = { authUser }
